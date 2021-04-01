a3aed3294e350f8923170c9e79742f1ed0bf2949
baosonnguyendang <baosonnguyendang@gmail.com>
2021-04-01 14:49:40 +0700

    danh dz

--- src/components/admin/admin-evaluate.js
+++ src/components/admin/admin-evaluate.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 
 import { Link } from 'react-router-dom';
 
-// import Modal from 'react-modal';  
+import DatePicker from 'react-datepicker';
 
 import Button from '@material-ui/core/Button';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import Modal from '@material-ui/core/Modal';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 function ListItem(props) {
   return <li>{props.value}</li>;
@@ -38,6 +42,11 @@ const numbers = [
 //create modal
 
 const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   paper: {
     position: 'absolute',
     width: 400,
@@ -51,30 +60,18 @@ const useStyles = makeStyles((theme) => ({
 export default function EvaluateList() {
   const classes = useStyles();
 
-  // const [modalStyle] = React.useState(getModalStyle);
+  //open modal
   const [open, setOpen] = React.useState(false);
-
   const handleOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
 
-  const modalStyle = {
-    backgroundColor: 'white'
-  }
-
-  const body = (
-    <div style={modalStyle}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <Button variant="contained" type="button" onClick={handleClose}>Lưu</Button>
-    </div>
-  );
+  //date picker
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
     <Paper>
@@ -84,12 +81,48 @@ export default function EvaluateList() {
       <NumberList numbers={numbers} />
       <Button variant="contained" type="button" onClick={handleOpen}>Thêm đợt đánh giá</Button>
       <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
         open={open}
         onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
       >
-        {body}
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Thêm đợt đánh giá</h2>
+            <form>
+              <TextField
+                // onChange={this.onChangeUsername}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="evaluateName"
+                label="Tên đợt đánh giá"
+                name="evaluateName"
+                autoComplete="evaluateName"
+                autoFocus
+              />
+              <label>Từ ngày:  &nbsp; </label>
+              <DatePicker selected={startDate} onChange={date => setStartDate(date)} selectsStart
+                startDate={startDate} endDate={endDate} dateFormat="dd/MM/yyyy"
+              />
+              <br />
+              <label>Đến ngày:&nbsp; </label>
+              <DatePicker selected={endDate} onChange={date => setEndDate(date)} selectsEnd
+                startDate={startDate} endDate={endDate} minDate={startDate} dateFormat="dd/MM/yyyy"
+              />
+              <br />
+              <Button type="submit" variant="contained" color="primary">Tạo</Button>
+              <Button variant="contained" color="primary" onClick={handleClose}>Hủy</Button>
+            </form>
+          </div>
+        </Fade>
       </Modal>
     </Paper>
   )
