a2fcb86ae741415935fd6492eb233b3aecc831db
baosonnguyendang <baosonnguyendang@gmail.com>
2021-05-10 17:57:57 +0700

    May 10, 5:57pm

--- src/components/admin/evaluate-setting/admin-drag-criterion.js
+++ src/components/admin/evaluate-setting/admin-drag-criterion.js
@@ -1,128 +1,138 @@
 import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-function PostButton(props) {
-  let style = {
-    margin: 10,
-    height: 30,
-  };
-  return (
-    <Button variant="contained" style={style} onClick={() => props.handleClick()}>{props.label}</Button>
-    // <button style={style} onClick={() => props.handleClick()}>{props.label}</button>
-  );
-}
+import { makeStyles } from '@material-ui/core/styles';
 
-function PostText(props) {
-  // let style = {
-  //   lineHeight: '30px',
-  //   marginTop: 10,
-  // };
-  return (
-    // <div style={style}><span>{props.text}</span></div>
-    <Typography style={ props.score ? {marginTop: 10, lineHeight: '30px'} : {textAlign: 'center'} }>{props.text}</Typography>
-  );
-}
-
-function Post(props) {
-  let style = {
-    display: "flex",
-  };
-  return (
-    // <div style={style}>
-    //   <PostButton label='x' handleClick={props.removeItem} />
-    //   <PostText text={props.title} style={{ width: '50%' }} />
-    //   <PostButton label='+' handleClick={props.incrementScore} />
-    //   <PostText text={props.score} width="20" />
-    //   <PostButton label='-' handleClick={props.decrementScore} />
-    // </div>
-    <tr>
-      <td><PostButton label='x' handleClick={props.removeItem} /></td>
-      <td><PostText text={props.title} /></td>
-      <td style={style}>
-        <PostButton label='+' handleClick={props.incrementScore} />
-        <PostText score={true} text={props.score} style={{lineHeight: 30}} />
-        <PostButton label='-' handleClick={props.decrementScore} />
-      </td>
-    </tr>
-  );
-}
-
-function PostList(props) {
-  return (
-    <table>
-      <tr>
-        <th></th>
-        <th>Tên tiêu ch</th>
-        <th style={{textAlign: 'center'}}>STT</th>
-
-      </tr>
-      {
-        props.postList.map((item, index) => (
-          <Post key={index}
-            title={item.title}
-            score={item.score}
-            incrementScore={() => props.updateScore(index, 1)}
-            decrementScore={() => props.updateScore(index, -1)}
-            removeItem={() => props.removeItem(index)}
-          />
-        ))
-      }
-    </table>
-  )
-}
-
-// function Post(props) {
-//   let style = {
-//     display: "flex"
-//   };
-//   return (
-//     <div style={style}>
-//       <PostButton label='x' handleClick={props.removeItem} />
-//       <PostText text={props.title} style={{ width: '50%' }} />
-//       <PostButton label='+' handleClick={props.incrementScore} />
-//       <PostText text={props.score} width="20" />
-//       <PostButton label='-' handleClick={props.decrementScore} />
-//     </div>
-//   );
-// }
-
-// function PostList(props) {
-//   return (
-//     <ol>
-//       {
-//         props.postList.map((item, index) => (
-//           <Post key={index}
-//             title={item.title}
-//             score={item.score}
-//             incrementScore={() => props.updateScore(index, 1)}
-//             decrementScore={() => props.updateScore(index, -1)}
-//             removeItem={() => props.removeItem(index)}
-//           />
-//         ))
-//       }
-//     </ol>
-//   )
-// }
-
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper1: {
+    position: 'absolute',
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    minHeight: 250,
+  },
+}))
 
 export default function Drag() {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: "",
-  //     items: []
-  //   }
-  // }
-  const [items, setItems] = useState([])
-  // const handleChange(event) {
-  //   this.setState({ value: event.target.value });
-  // }
+  const classes = useStyles()
 
+  //cái này là để mở cái modal chỉnh tiêu chí
+  const [openCriteria, setOpenCriteria] = React.useState(false);
+  const handleCloseCriteria = () => {
+    setOpenCriteria(false);
+  }
+
+  const PostButton = (props) => {
+    let style = {
+      margin: 10,
+      height: 30,
+    };
+
+    return (
+      <Button variant="contained" style={style} onClick={() => props.handleClick()}>{props.label}</Button>
+    );
+  }
+
+  const PostText = (props) => {
+    return (
+      <Typography style={props.score ? { marginTop: 10, lineHeight: '30px' } : { textAlign: 'center' }}>{props.text}</Typography>
+    );
+  }
+
+  //id tieu chuan duoc chon
+  const [chosen, setChosen] = useState()
+
+  const Post = (props) => {
+    let style = {
+      display: "flex",
+    };
+
+    const handleOpen = (x) => {
+      setOpenCriteria(true)
+      setChosen(x)
+    }
+
+    return (
+      <tr>
+        <td><PostButton label='x' handleClick={props.removeItem} /></td>
+        <td><span type='button' onClick={() => {handleOpen(props.code)}}><PostText text={props.title} /></span></td>
+        <td style={style}>
+          <PostButton label='+' handleClick={props.incrementScore} />
+          <PostText score={true} text={props.score} style={{ lineHeight: 30 }} />
+          <PostButton label='-' handleClick={props.decrementScore} />
+        </td>
+      </tr>
+    );
+  }
+
+  const PostList = (props) => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th style={{ textAlign: 'center' }}>Tên tiêu ch</th>
+            <th style={{ textAlign: 'center' }}>STT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.postList.map((item, index) => (
+              <Post key={index}
+                title={item.title}
+                score={item.score}
+                code={item.code}
+                incrementScore={() => props.updateScore(index, 1)}
+                decrementScore={() => props.updateScore(index, -1)}
+                removeItem={() => props.removeItem(index)}
+              />
+            ))
+          }
+        </tbody>
+      </table >
+    )
+  }
+
+  //lấy nhẹ data tiêu chuẩn từ db
+  const [data, setData] = React.useState([])
+
+  const token = localStorage.getItem('token')
+  const fetchCriterion = () => {
+    axios.get('admin/standard/criteria', { headers: { "Authorization": `Bearer ${token}` } })
+      .then(res => {
+        setData(res.data.standards)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  useEffect(() => {
+    if (data.length == 0) {
+      fetchCriterion()
+    }
+  })
+
+  const [items, setItems] = useState([])
+
+  //selection
   const [newValue, setNewValue] = React.useState('');
   const handleChangeValue = (event) => {
     setNewValue(event.target.value);
@@ -130,10 +140,10 @@ export default function Drag() {
 
   const addItem = () => {
     let itemsCopy = items.slice();
-    let truncatedString = newValue;
-    itemsCopy.push({ "title": truncatedString, "score": 0 })
+    let truncatedString = data.find(x => x.code == newValue).name; //data.find(x => x.code == value).name
+    itemsCopy.push({ "title": truncatedString, "score": 1, "code": newValue })
     itemsCopy.sort((a, b) => {
-      return b.score - a.score;
+      return a.score - b.score;
     })
     setItems(itemsCopy)
     setNewValue('')
@@ -142,11 +152,14 @@ export default function Drag() {
 
   const updateScore = (index, val) => {
     let itemsCopy = items.slice();
-    itemsCopy[index].score += val;
+    if ((itemsCopy[index].score < items.length && val > 0) || (itemsCopy[index].score > 1 && val < 0)) {
+      itemsCopy[index].score += val
+    }
     itemsCopy.sort((a, b) => {
-      return b.score - a.score;
+      return a.score - b.score;
     })
     setItems(itemsCopy)
+    console.log(items)
     // this.setState({ items: itemsCopy });
   }
 
@@ -154,7 +167,7 @@ export default function Drag() {
     var itemsCopy = items.slice();
     itemsCopy.splice(index, 1);
     itemsCopy.sort((a, b) => {
-      return b.score - a.score;
+      return a.score - b.score;
     });
     setItems(itemsCopy)
     // this.setState({ items: itemsCopy });
@@ -163,7 +176,7 @@ export default function Drag() {
   return (
     <div>
       <FormControl variant="outlined" >
-        <InputLabel >Đơn vị</InputLabel>
+        <InputLabel >Tiêu chuẩn</InputLabel>
         <Select
           native
           value={newValue}
@@ -171,17 +184,37 @@ export default function Drag() {
           onChange={handleChangeValue}
         >
           <option aria-label="None" value="" />
-          <option value={10}>Định mức giờ chuẩn hoàn thành</option>
-          <option value={20}>Kết quả khảo sát chất lượng dịch vụ</option>
-          <option value={30}>Hình thức giảng dạy khác</option>
+          {data.map(x => {
+            return (
+              <option key={x._id} value={x.code}>{x.name}</option>
+            )
+          })}
         </Select>
       </FormControl>
-      {/* <input value={this.state.value} onChange={this.handleChange.bind(this)} /> */}
-      {/* <button onClick={() => addItem()}>Thêm</button> */}
       <Button style={{ marginLeft: 10, height: 56 }} variant="contained" color="primary" onClick={() => addItem()}>Thêm tiêu ch</Button>
       <PostList postList={items}
         updateScore={updateScore}
-        removeItem={removeItem} />
+        removeItem={removeItem}
+      />
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={openCriteria}
+        onClose={handleCloseCriteria}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openCriteria}>
+          <div className={classes.paper1}>
+            <h4 id="transition-modal-title">Thêm tiêu chí cho tiêu chuẩn {chosen}</h4>
+            <Button style={{ marginTop: '10px' }} variant="contained" color="primary" onClick={handleCloseCriteria}>Xong</Button>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
 }
