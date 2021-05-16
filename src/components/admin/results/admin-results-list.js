614b71680fc09d679c79501f953f2972f6062e00
baosonnguyendang <baosonnguyendang@gmail.com>
2021-05-16 19:27:26 +0700

    May 16, 7:27pm, xem ket qua danh gia

--- src/components/admin/results/admin-results-list.js
+++ src/components/admin/results/admin-results-list.js
@@ -30,33 +30,40 @@ const useStyles = makeStyles(theme => ({
 
 export default function ResultsList(props) {
   const classes = useStyles()
-
   const { id, id1 } = useParams()
-
   const token = localStorage.getItem('token')
+  const [units, setUnits] = useState([]) //ds đơn vị trong đợt đánh giá
 
-  // axios.get(`/admin/form/${code}/getFormDepartments`, { headers: { "Authorization": `Bearer ${token}` } })
-  //   .then(res => {
-  //     console.log(res)
-  //   })
-  //   .catch(e => console.log(e))
+  //mã form
+  // const [code, setCode] = useState()
+  var code = null
 
-  // axios.get(`/admin/review/${id}/formtype/${id1}/form/`, { headers: { "Authorization": `Bearer ${token}` } })
-  //   .then(res => {
-  //     // (res.data) && (setInit(false))
-  //     if (res.data.form) {
-  //       code = res.data.form.code
-  //       name = res.data.form.name
-  //     }
-  //   })
-  //   .catch(e => {
-  //     console.log(e)
-  //   })
+  //lấy mã form
+  axios.get(`/admin/review/${id}/formtype/${id1}/form/`, { headers: { "Authorization": `Bearer ${token}` } })
+    .then(res => {
+      if (res.data.form) {
+        // setCode(res.data.form.code)
+        code = res.data.form.code
+        //lấy các đơn vị cha nằm trong đợt đánh giá
+        axios.get(`/admin/form/${code}/getFormDepartments`, { headers: { "Authorization": `Bearer ${token}` } })
+          .then(res => {
+            let _id = res.data.formDepartments.filter(x => (x.level === 1 || x.level === 0))
+            console.log(_id)
+          })
+          .catch(e => console.log(e))
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    })
+
+
+
 
   return (
     <div>
       <Typography component="h1" variant="h5" color="inherit" noWrap>
-        Đợt {id} - Nhóm {id1}
+        Đợt {id} - Nhóm {id1} - Mã Form: {code}
       </Typography>
       <Paper className={classes.paper}>
         <Typography component="h4" variant="h6" color="inherit" noWrap>
