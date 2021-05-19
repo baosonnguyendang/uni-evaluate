76ab28b570184db7ee915aa655c69ecd77648e3f
baosonnguyendang <baosonnguyendang@gmail.com>
2021-05-19 15:01:46 +0700

    May 19, 3:01pm, fix ti giao dien user

--- src/components/user/user-form-list.js
+++ src/components/user/user-form-list.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
+
+import { Card, CardActions, CardContent, Typography } from '@material-ui/core'
+
 import { Link, useRouteMatch, useParams } from 'react-router-dom'
 
-export default function FormList(){
-  const {id} = useParams()
+export default function FormList() {
+  const { id } = useParams()
   const token = localStorage.getItem('token')
   const { url } = useRouteMatch()
 
@@ -16,7 +19,7 @@ export default function FormList(){
         let temp = []
         console.log(res.data.formUser)
         res.data.formUser.map(x => {
-          let obj = {code: x.form_id.code, name: x.form_id.name, id: x.form_id._id}
+          let obj = { code: x.form_id.code, name: x.form_id.name, id: x.form_id._id }
           temp.push(obj)
         })
         setList(temp)
@@ -29,7 +32,19 @@ export default function FormList(){
   return (
     list.map(x => {
       return (
-        <Link key={x.code} to={`${url}/${x.id}`}>{x.name}</Link>
+        <Card style={{marginTop: '24px'}}>
+          <CardContent>
+            <Typography>
+              Tên Form: {x.name}
+            </Typography>
+            <Typography>
+              Mô tả:
+            </Typography>
+          </CardContent>
+          <CardActions style={{paddingLeft: '16px'}}>
+            <Link key={x.code} to={`${url}/${x.id}`}>Bấm vào đây để thực hiện đánh giá..</Link>
+          </CardActions>
+        </Card>
       )
     })
   )
