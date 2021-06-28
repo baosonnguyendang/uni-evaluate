c1d8f213c10bb3b68ebef52014a792aafced385e
Danh Nguyen <congdanhnguyen116@gmail.com>
2021-06-28 23:49:32 +0700

    add validate submit excel

--- src/components/common/UpLoadFile.js
+++ src/components/common/UpLoadFile.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Button, Typography } from '@material-ui/core'
 
 const FileUploader = props => {
   const hiddenFileInput = React.useRef(null);
-  const [file, setFile] = React.useState({})
+  const [file, setFile] = React.useState(null)
+  const [error, setError] = useState(null)
 
   const handleClick = event => {
     hiddenFileInput.current.click();
   };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!file) {
+      setError("Vui lòng chọn file excel")
+      return
+    }
+    props.submit(file)
+    setError(null)
+    setFile(null)
+  }
   const handleChange = event => {
     setFile(event.target.files[0])
   };
-
   return (
     <>
       <Typography variant='h5' gutterBottom >{props.title}</Typography>
-      <form  >
+      <form onSubmit={handleSubmit} >
         <Button onClick={handleClick} variant="contained" size="small">
           Chọn file excel
         </Button>
@@ -25,13 +35,15 @@ const FileUploader = props => {
           ref={hiddenFileInput}
           onChange={handleChange}
           style={{ display: 'none' }}
+          name='file'
         />
         <div style={{ height: '150px' }}>
-          <Typography>{file.name}</Typography>
+          {error && <Typography color='secondary'>{error}</Typography>}
+          <Typography>{file?.name}</Typography>
         </div>
         <br />
         <div style={{ textAlign: 'center', marginTop: '10px' }}>
-          <Button style={{ marginRight: '10px' }} variant="contained" color="primary" onClick={() => props.submit(file)} >Import</Button>
+          <Button style={{ marginRight: '10px' }} type="submit" variant="contained" color="primary"  >Import</Button>
           <Button style={{ marginLeft: '10px' }} variant="contained" color="primary" onClick={props.handleClose}>Thoát</Button>
         </div>
       </form>
