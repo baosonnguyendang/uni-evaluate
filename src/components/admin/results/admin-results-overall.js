4ff461e5d4cc8a0faba37b6ac04a41d96e294715
baosonnguyendang <baosonnguyendang@gmail.com>
2021-07-02 12:27:07 +0700

    July 2,12:26pm, sua theo yeu cau dzanh

--- src/components/admin/results/admin-results-overall.js
+++ src/components/admin/results/admin-results-overall.js
@@ -241,7 +241,7 @@ export default function ResultsList(props) {
               <Card className={classes.root}>
                 <CardContent>
                   <Typography align='center' variant='h2' color="textSecondary" gutterBottom>
-                    {point.length > 0 ? Math.max(...point) : '!'}
+                    {point.length > 0 ? Math.max(...point) : 'null'}
                   </Typography>
                   <Typography align='center' variant="body2" component="p">
                     Điểm số cao nhất trong Form
@@ -251,7 +251,7 @@ export default function ResultsList(props) {
               <Card className={classes.root}>
                 <CardContent>
                   <Typography align='center' variant='h2' color="textSecondary" gutterBottom>
-                    {point.length > 0 ? Math.max(...point) : '!'}
+                    {point.length > 0 ? Math.max(...point) : 'null'}
                   </Typography>
                   <Typography align='center' variant="body2" component="p">
                     Điểm số thấp nhất trong Form
