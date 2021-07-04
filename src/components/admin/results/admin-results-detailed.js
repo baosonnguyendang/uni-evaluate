2f2a897b88b157e551561ac8aab87abe0a91732e
baosonnguyendang <baosonnguyendang@gmail.com>
2021-07-04 23:36:56 +0700

    July 4, 11:35pm, sua form ben admin

--- src/components/admin/results/admin-results-detailed.js
+++ src/components/admin/results/admin-results-detailed.js
@@ -6,6 +6,10 @@ import { LinearProgress, Typography, TableContainer, Table, TableHead, TableRow,
 
 import { useParams } from 'react-router-dom'
 
+import { useDispatch } from 'react-redux'
+
+import { showModal } from '../../../actions/modalAction'
+
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
@@ -14,6 +18,7 @@ const useStyles = makeStyles({
 
 export default function ResultsDetailed() {
   const classes = useStyles();
+  const dispatch = useDispatch()
   const token = localStorage.getItem('token')
   const { id3 } = useParams()
 
@@ -82,6 +87,26 @@ export default function ResultsDetailed() {
       })
   }, [])
 
+  const sendDetails = (criteria, level) => {
+    console.log(level)
+    switch (level) {
+      case 1:
+        return (
+          data[0].find(x => x.name == criteria).details ? data[0].find(x => x.name == criteria).details : []
+        )
+      case 2:
+        return (
+          data[1].find(x => x.name == criteria).details ? data[1].find(x => x.name == criteria).details : []
+        )
+      case 3:
+        return (
+          data[2].find(x => x.name == criteria).details ? data[2].find(x => x.name == criteria).details : []
+        )
+      default:
+        return null
+    }
+  }
+
   return (
     <div>
       <div>
@@ -121,7 +146,7 @@ export default function ResultsDetailed() {
                             <TableCell><b>{criteria.criteria_id.name}</b></TableCell>
                             <TableCell>{criteria.point}</TableCell>
                             <TableCell align='center'>
-                              {criteria.options.length > 0 ? null : (criteria.criteria_id.type != 'checkbox' ? (
+                              {/* {criteria.options.length > 0 ? null : (criteria.criteria_id.type != 'checkbox' ? (
                                 <input
                                   className='number'
                                   type="number"
@@ -138,45 +163,164 @@ export default function ResultsDetailed() {
                                   checked={data.length > 0 && data[0].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
                                   value={criteria.point}
                                 />
-                              ))}
+                              ))} */}
+                              {criteria.options.length > 0 ? null : (() => {
+                                switch (criteria.criteria_id.type) {
+                                  default:
+                                    return null
+                                  case 'input':
+                                    return (
+                                      <input
+                                        className='number'
+                                        type="number"
+                                        style={{ width: '40px', textAlign: 'center' }}
+                                        disabled
+                                        value={data.length > 0 && data[0].find(y => (y.name == criteria.criteria_id.code)).value}
+                                        name={criteria.criteria_id.code + '_1'}
+                                      />
+                                    )
+                                  case 'checkbox':
+                                    return (
+                                      <input
+                                        type="checkbox"
+                                        disabled
+                                        checked={data.length > 0 && data[0].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
+                                        name={criteria.criteria_id.code + '_1'}
+                                        value={criteria.point}
+                                      />
+                                    )
+                                  case 'detail':
+                                    return (
+                                      <input
+                                        style={{ width: 40 }}
+                                        type='button'
+                                        onClick={() => {
+                                          dispatch(showModal(data => console.log(data), "DETAIL_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) }))
+                                        }}
+                                        value={data[0].find(x => x.name == criteria.criteria_id.code) ? data[0].find(x => x.name == criteria.criteria_id.code).value : 0}
+                                        onMouseUp={e => e.target.blur()}
+                                      />
+                                    )
+                                  case 'number':
+                                    return (
+                                      <div>
+                                        <input
+                                          style={{ width: 40 }}
+                                          type='button'
+                                          onClick={() => { dispatch(showModal(data => console.log(data), "TIMES_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) })) }}
+                                          value={data[0].find(x => x.name == criteria.criteria_id.code) ? data[0].find(x => x.name == criteria.criteria_id.code).value : 0}
+                                          onMouseUp={e => e.target.blur()}
+                                        />
+                                      </div>
+                                    )
+                                }
+                              })()}
                             </TableCell>
                             <TableCell align='center'>
-                              {criteria.options.length > 0 ? null : (criteria.criteria_id.type != 'checkbox' ? (
-                                <input
-                                  className='number'
-                                  type="number"
-                                  style={{ width: '40px', textAlign: 'center' }}
-                                  disabled
-                                  value={data.length > 0 && data[1].find(y => (y.name == criteria.criteria_id.code)).value}
-                                  name={criteria.criteria_id.code + '_2'}
-                                />
-                              ) : (
-                                <input
-                                  disabled
-                                  checked={data.length > 1 && data[1].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
-                                  type="checkbox"
-                                  name={criteria.criteria_id.code + '_2'}
-                                  value={criteria.point} />
-                              ))}
+                              {criteria.options.length > 0 ? null : (() => {
+                                switch (criteria.criteria_id.type) {
+                                  default:
+                                    return null
+                                  case 'input':
+                                    return (
+                                      <input
+                                        className='number'
+                                        type="number"
+                                        style={{ width: '40px', textAlign: 'center' }}
+                                        disabled
+                                        value={data.length > 1 && data[1].find(y => (y.name == criteria.criteria_id.code)).value}
+                                        name={criteria.criteria_id.code + '_2'}
+                                      />
+                                    )
+                                  case 'checkbox':
+                                    return (
+                                      <input
+                                        type="checkbox"
+                                        disabled
+                                        checked={data.length > 1 && data[1].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
+                                        name={criteria.criteria_id.code + '_2'}
+                                        value={criteria.point}
+                                      />
+                                    )
+                                  case 'detail':
+                                    return (
+                                      <input
+                                        style={{ width: 40 }}
+                                        type='button'
+                                        onClick={() => {
+                                          dispatch(showModal(data => console.log(data), "DETAIL_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) }))
+                                        }}
+                                        value={data.length > 1 ? (data[1].find(x => x.name == criteria.criteria_id.code) ? data[1].find(x => x.name == criteria.criteria_id.code).value : 0) : ''}
+                                        onMouseUp={e => e.target.blur()}
+                                      />
+                                    )
+                                  case 'number':
+                                    return (
+                                      <div>
+                                        <input
+                                          style={{ width: 40 }}
+                                          type='button'
+                                          onClick={() => { dispatch(showModal(data => console.log(data), "TIMES_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) })) }}
+                                          value={data.length > 1 ? (data[1].find(x => x.name == criteria.criteria_id.code) ? data[1].find(x => x.name == criteria.criteria_id.code).value : 0) : ''}
+                                          onMouseUp={e => e.target.blur()}
+                                        />
+                                      </div>
+                                    )
+                                }
+                              })()}
                             </TableCell>
                             <TableCell align='center'>
-                              {criteria.options.length > 0 ? null : (criteria.criteria_id.type != 'checkbox' ? (
-                                <input
-                                  className='number'
-                                  type="number"
-                                  style={{ width: '40px', textAlign: 'center' }}
-                                  disabled
-                                  value={data.length > 2 && data[2].find(y => (y.name == criteria.criteria_id.code)).value}
-                                  name={criteria.criteria_id.code + '_3'}
-                                />
-                              ) : (
-                                <input
-                                  disabled
-                                  checked={data.length > 2 && data[2].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
-                                  type="checkbox"
-                                  name={criteria.criteria_id.code + '_3'}
-                                  value={criteria.point} />
-                              ))}
+                              {criteria.options.length > 0 ? null : (() => {
+                                switch (criteria.criteria_id.type) {
+                                  default:
+                                    return null
+                                  case 'input':
+                                    return (
+                                      <input
+                                        className='number'
+                                        type="number"
+                                        style={{ width: '40px', textAlign: 'center' }}
+                                        disabled
+                                        value={data.length > 2 && data[2].find(y => (y.name == criteria.criteria_id.code)).value}
+                                        name={criteria.criteria_id.code + '_3'}
+                                      />
+                                    )
+                                  case 'checkbox':
+                                    return (
+                                      <input
+                                        type="checkbox"
+                                        disabled
+                                        checked={data.length > 2 && data[2].find(y => (y.name == criteria.criteria_id.code && y.value == criteria.point))}
+                                        name={criteria.criteria_id.code + '_3'}
+                                        value={criteria.point}
+                                      />
+                                    )
+                                  case 'detail':
+                                    return (
+                                      <input
+                                        style={{ width: 40 }}
+                                        type='button'
+                                        onClick={() => {
+                                          dispatch(showModal(data => console.log(data), "DETAIL_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) }))
+                                        }}
+                                        value={data.length > 2 ? (data[2].find(x => x.name == criteria.criteria_id.code) ? data[2].find(x => x.name == criteria.criteria_id.code).value : 0) : ''}
+                                        onMouseUp={e => e.target.blur()}
+                                      />
+                                    )
+                                  case 'number':
+                                    return (
+                                      <div>
+                                        <input
+                                          style={{ width: 40 }}
+                                          type='button'
+                                          onClick={() => { dispatch(showModal(data => console.log(data), "TIMES_MODAL", { disableEdit: true, name: criteria.criteria_id.name, code: criteria.criteria_id.code, max_point: criteria.point ? criteria.point : null, base_point: criteria.base_point, details: sendDetails(criteria.criteria_id.code, 1) })) }}
+                                          value={data.length > 2 ? (data[2].find(x => x.name == criteria.criteria_id.code) ? data[2].find(x => x.name == criteria.criteria_id.code).value : 0) : ''}
+                                          onMouseUp={e => e.target.blur()}
+                                        />
+                                      </div>
+                                    )
+                                }
+                              })()}
                             </TableCell>
                           </TableRow>
                           {criteria.options.map((option, index) => (
